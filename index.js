7d0f0b16cd28e3da70748ae0ef5051697b453799
risano <muhammadrisano@gmail.com>
2021-08-27 16:56:41 +0700

    group chat

--- index.js
+++ index.js
@@ -25,8 +25,43 @@ const io = socket(httpServer, {
 // use socket
 io.on('connection', (socket)=>{
   console.log('ada client yg terhubung', socket.id);
+  // socket.on('sendMsgToBack', (data)=>{
+  //   console.log(data);
+  //   // kirim ke diri sendiri
+  //   // socket.emit('sendMsgToFront', 'hallo world')
+
+  //   // io.emit
+  //   // kirim kesemua user yg terkoneksi
+  //   io.emit('sendMsgToFront', 'hallo world')
+
+  //   // socket.broadcas.emit
+  //   // kirim kesemua user yg terkoneksi kecuali user yg itu sendiri
+  //   socket.broadcast.emit('sendMsgToFront', 'hallo world')
+
+
+  //   // socket.on -> listen /mendengar event
+  // })
+
+  socket.on('initialGroup', ({group, email})=>{
+    console.log('ada user yg join group '+ group);
+    socket.join(`group:${group}`)
+    socket.broadcast.to(`group:${group}`).emit('sendMsgFromBackend', { email: 'admin', message: `user ${email} join group`, group: group} )
+  })
+
+  socket.on('sendMessage', (data) => {
+    console.log(data);
+    // saya panggil controller dan model untuk simpan ke db
+    
+    io.to(`group:${data.group}`).emit('sendMsgFromBackend', data)
+
+  })
+  
+  
+  
+  
   socket.on('disconnect', ()=>{
     console.log('ada perangkat yang terputus ', socket.id);
+    io.emit('sendMsgFromBackend', { email: 'admin', message: `ada user yg keluar dari aplikasi` })
   })
 })
 
